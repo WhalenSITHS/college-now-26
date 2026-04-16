c227beab52edee7438d0fd0e3c7134c7fce1e877
mr. whalen <michael.whalen@sitechhs.com>
2026-04-16 15:50:02 -0400

    ready

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,6 +82,16 @@ exports.register = async (req, res) => {
   }
 };
 
+exports.getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
